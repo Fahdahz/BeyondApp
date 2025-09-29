ef1da2abf853ffd19590a8a12f654ee1e54f0bf2
Fahdahz <Fahdah.zms@gmail.com>
2025-09-29 10:56:20 +0300

    Initial Commit

---
+++ BeyondApp/BeyondAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeyondAppApp.swift
+//  BeyondApp
+//
+//  Created by Fahdah Alsamari on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct BeyondAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeyondAppTests/BeyondAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeyondAppTests.swift
+//  BeyondAppTests
+//
+//  Created by Fahdah Alsamari on 07/04/1447 AH.
+//
+
+import Testing
+@testable import BeyondApp
+
+struct BeyondAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
